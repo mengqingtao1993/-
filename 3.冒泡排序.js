5770592a9e4337ab2b4d8831da7c3a18fe44bc91
mengqingtao <mengqingtao@51shebao.com>
2021-09-14 20:46:05 +0800

    冒泡排序

---
+++ 3.冒泡排序.js
@@ -0,0 +1,23 @@
+/**
+ * 外层循环:负责控制冒到哪里
+ * 内层循环:负责具体冒上去
+ */
+let arr = [33, 43, 21, 6, 22, 7, 96, 4, 32, 5]
+function bubble_sort (A) {
+  // i指当前轮次最大位置
+  for (let i = A.length - 1; i > 0; i--) {
+    // j指当前元素
+    for (let j = 0; j <= i; j++) {
+      if (A[j] > A[j + 1]) {
+        swap(A, j, j + 1)
+      }
+    }
+  }
+}
+function swap (A, i, j) {
+  let t = A[i]
+  A[i] = A[j]
+  A[j] = t
+}
+bubble_sort(arr)
+console.log(arr)
